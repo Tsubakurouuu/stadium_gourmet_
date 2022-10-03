d8f20ab21bbb5d95363be723ffe73c058df264c3
Tsubakurouuu <yzpolice2896@icloud.com>
2022-10-03 08:42:48 +0000

    [Add]seeds.rd初期データ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#カテゴリーモデルのデータ作成
+Category.create(name: "弁当")
+Category.create(name: "丼")
+Category.create(name: "麺類")
+Category.create(name: "ファストフード")
+Category.create(name: "スイーツ")
+Category.create(name: "ドリンク")
+
+#販売エリアモデルのデータ作成
+SaleArea.create(name: "バックネット裏")
+SaleArea.create(name: "一塁側")
+SaleArea.create(name: "三塁側")
+SaleArea.create(name: "レフト側")
+SaleArea.create(name: "ライト側")
